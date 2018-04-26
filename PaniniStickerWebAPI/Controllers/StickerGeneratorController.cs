fb18e556d64536641630a30befd4cb601f0b0bfc
Aldo Fernando Vilardy Roa <aldovilardy@outlook.com>
2018-04-26 14:02:51 -0500

    First commit

---
+++ PaniniStickerWebAPI/Controllers/StickerGeneratorController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PaniniWebAPI.Controllers
+{
+    public class StickerGeneratorController : ApiController
+    {
+        public HttpResponseMessage Post(Models.StickerRequest stickerRequest)
+        {
+
+            return new HttpResponseMessage(HttpStatusCode.Accepted);
+        }
+    }
+}
---
+++ PaniniStickerWebAPI/Models/StickerRequest.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PaniniWebAPI.Models
+{
+    public class StickerRequest
+    {
+        [JsonProperty("PhotoUrl")]
+        public string PhotoUrl { get; set; }
+        [JsonProperty("Frame")]
+        public string Frame { get; set; }
+        [JsonProperty("FullName")]
+        public string FullName { get; set; }
+        [JsonProperty("Position")]
+        public string Position { get; set; }
+        [JsonProperty("DoB")]
+        public DateTime DateOfBirthday { get; set; }
+        [JsonProperty("Club")]
+        public string Club { get; set; }
+        [JsonProperty("Country")]
+        public string Country { get; set; }
+        [JsonProperty("Debut")]
+        public short Debut { get; set; }
+    }
+}
